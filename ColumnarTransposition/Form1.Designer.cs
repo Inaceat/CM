b3582574419dda2425a5288632b820d70824e468
Inaceat <inaceat@gmail.com>
2020-03-10 14:03:47 +0300

    Columnar transposition (Lab 1) added.

---
+++ ColumnarTransposition/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ColumnarTransposition
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plainTextBox = new System.Windows.Forms.TextBox();
+            this.cipherTextBox = new System.Windows.Forms.TextBox();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.rowsTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.columnsTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // plainTextBox
+            // 
+            this.plainTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.plainTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.plainTextBox.Location = new System.Drawing.Point(12, 12);
+            this.plainTextBox.Multiline = true;
+            this.plainTextBox.Name = "plainTextBox";
+            this.plainTextBox.Size = new System.Drawing.Size(770, 201);
+            this.plainTextBox.TabIndex = 0;
+            // 
+            // cipherTextBox
+            // 
+            this.cipherTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cipherTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.cipherTextBox.Location = new System.Drawing.Point(12, 288);
+            this.cipherTextBox.Multiline = true;
+            this.cipherTextBox.Name = "cipherTextBox";
+            this.cipherTextBox.Size = new System.Drawing.Size(770, 201);
+            this.cipherTextBox.TabIndex = 1;
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.encryptButton.Location = new System.Drawing.Point(243, 219);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(75, 63);
+            this.encryptButton.TabIndex = 2;
+            this.encryptButton.Text = "Encrypt \\/";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.encryptButton_Click);
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.decryptButton.Location = new System.Drawing.Point(452, 219);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(75, 63);
+            this.decryptButton.TabIndex = 3;
+            this.decryptButton.Text = "Decrypt /\\";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            this.decryptButton.Click += new System.EventHandler(this.decryptButton_Click);
+            // 
+            // rowsTextBox
+            // 
+            this.rowsTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.rowsTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rowsTextBox.Location = new System.Drawing.Point(324, 241);
+            this.rowsTextBox.Name = "rowsTextBox";
+            this.rowsTextBox.Size = new System.Drawing.Size(47, 22);
+            this.rowsTextBox.TabIndex = 4;
+            this.rowsTextBox.Text = "2";
+            this.rowsTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(377, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "X";
+            // 
+            // columnsTextBox
+            // 
+            this.columnsTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.columnsTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.columnsTextBox.Location = new System.Drawing.Point(399, 241);
+            this.columnsTextBox.Name = "columnsTextBox";
+            this.columnsTextBox.Size = new System.Drawing.Size(47, 22);
+            this.columnsTextBox.TabIndex = 7;
+            this.columnsTextBox.Text = "3";
+            this.columnsTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 501);
+            this.Controls.Add(this.columnsTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rowsTextBox);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.encryptButton);
+            this.Controls.Add(this.cipherTextBox);
+            this.Controls.Add(this.plainTextBox);
+            this.Name = "MainForm";
+            this.Text = "Columnar transposition";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox plainTextBox;
+        private System.Windows.Forms.TextBox cipherTextBox;
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.TextBox rowsTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox columnsTextBox;
+    }
+}
+
---
+++ ColumnarTransposition/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace ColumnarTransposition
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void encryptButton_Click(object sender, EventArgs e)
+        {
+            ushort rows;
+            ushort columns;
+            try
+            {
+                rows = ushort.Parse(rowsTextBox.Text);
+                columns = ushort.Parse(columnsTextBox.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Wrong rectangle size!");
+                return;
+            }
+
+
+            //Remove all but letters & digits
+            var letters = Regex.Replace(plainTextBox.Text, "[^a-zA-Z0-9а-яёА-ЯЁ]", "");
+
+            cipherTextBox.Text = Encrypt(letters, rows, columns);
+        }
+
+        private static string Encrypt(string inputText, int rows, int columns)
+        {
+            var convertingRectangleSize = rows * columns;
+
+            //Cut input to fit converting rectangle
+            if (inputText.Length > convertingRectangleSize)
+                inputText = inputText.Substring(0, convertingRectangleSize);
+
+
+            var convertedText = new StringBuilder(inputText.Length);
+
+            //Iterate columns, "left to right" in converting rectangle
+            for (var columnIndex = 0; columnIndex < columns; columnIndex++)
+            {
+                //In each column, iterate "down"
+                for (var rowIndex = 0; rowIndex < rows; rowIndex++)
+                {
+                    var currentInputIndex = rowIndex * columns + columnIndex;
+
+                    //If input text has char at this index, get it to result
+                    if (inputText.Length > currentInputIndex)
+                        convertedText.Append(inputText[currentInputIndex]);
+                    else
+                        //Move to next column
+                        break;
+                }
+            }
+
+            return convertedText.ToString();
+        }
+
+
+        private void decryptButton_Click(object sender, EventArgs e)
+        {
+            ushort rows;
+            ushort columns;
+            try
+            {
+                rows = ushort.Parse(rowsTextBox.Text);
+                columns = ushort.Parse(columnsTextBox.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Wrong rectangle size!");
+                return;
+            }
+            
+
+            var letters = Regex.Replace(cipherTextBox.Text, "[^a-zA-Z0-9а-яёА-ЯЁ]", "");
+            
+            plainTextBox.Text = Decrypt(letters, rows, columns);
+        }
+
+        private static string Decrypt(string inputText, int rows, int columns)
+        {
+            //While decrypting, transposed rectangle is used
+            (rows, columns) = (columns, rows);
+
+            var convertingRectangleSize = rows * columns;
+
+            //Cut input to fit converting rectangle
+            if (inputText.Length > convertingRectangleSize)
+                inputText = inputText.Substring(0, convertingRectangleSize);
+
+            //Result storage
+            var convertedText = new char[inputText.Length];
+
+            using (var inputChar = inputText.GetEnumerator())
+            {
+                //Iterate rectangle
+                for (var rowIndex = 0; rowIndex < rows; rowIndex++)
+                {
+                    for (var columnIndex = 0; columnIndex < columns; columnIndex++)
+                    {
+                        //If current [row, column] rectangle cell should contain letter
+                        if (columnIndex * rows + rowIndex < inputText.Length)
+                        {
+                            //Get next input char
+                            inputChar.MoveNext();
+                            //And move to appropriate place in output
+                            convertedText[columnIndex * rows + rowIndex] = inputChar.Current;
+                        }
+                    }
+                }
+            }
+
+            return new string(convertedText);
+        }
+    }
+}
