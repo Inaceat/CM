3f92c22292a752130dec2593c30bb0da79cfdd0c
Inaceat <inaceat@gmail.com>
2020-03-13 15:57:44 +0300

    Viginere cipher now decodes.

--- VigenereCipher/MainForm.cs
+++ VigenereCipher/MainForm.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace VigenereCipher
 {
     public partial class MainForm : Form
     {
-        private static string russianAlphabet = ""
+        private const string RussianAlphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 
         public MainForm()
         {
@@ -21,7 +16,33 @@ namespace VigenereCipher
 
         private void decodeButton_Click(object sender, EventArgs e)
         {
+            plainTextBox.Text = DecodeViginere(cipherTextBox.Text, keyTextBox.Text, RussianAlphabet);
+        }
 
+        private string DecodeViginere(string text, string key, string alphabet)
+        {
+            if (!key.All(alphabet.Contains))
+                return "";
+
+            var result = new StringBuilder(text.Length);
+
+            var i = 0;
+            foreach (var symbol in text)
+            {
+                var symbolIndex = alphabet.IndexOf(char.ToLower(symbol));
+                
+                if (-1 != symbolIndex)
+                {
+                    var keyIndex = alphabet.IndexOf(key[i % key.Length]);
+                    ++i;
+                    result.Append(
+                        alphabet[(symbolIndex - keyIndex + alphabet.Length) % alphabet.Length]);
+                }
+                else
+                    result.Append(symbol);
+            }
+
+            return result.ToString();
         }
     }
 }
