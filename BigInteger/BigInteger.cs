95f5784c7e9768363a0ec1ceda595d4dc0ebab6b
Inaceat <inaceat@gmail.com>
2020-03-27 16:01:58 +0300

    [WIP]BigInt conversion from string in progress.

--- BigInteger/BigInteger.cs
+++ BigInteger/BigInteger.cs
@@ -6,14 +6,30 @@ namespace BigInteger
 {
     public class BigInteger
     {
-        private static readonly Regex NumberRegex = new Regex(@"^(?:\+|-)?[0-9]+$");
+        private static readonly Regex NumberRegex = new Regex(@"^(\+|-)?[0-9]+$");
+
+
+        private List<uint> _digits;
+        private bool _isPositive;
 
         public BigInteger(string number)
         {
             if (!NumberRegex.IsMatch(number))
                 throw new ArgumentException();
 
+            if (0 == NumberRegex.Matches(number)[0].Groups.Count ||
+                "+" == NumberRegex.Matches(number)[0].Groups[1].Value)
+                _isPositive = true;
 
+            
+            _digits = new List<uint>();
+            
+            _digits.Add(0);
+
+            for (var i = number.Length - 1; i >= 0; ++i)
+            {
+                
+            }
         }
 
         public override string ToString()
